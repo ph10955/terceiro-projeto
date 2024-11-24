5f6e91cef6c475305591e86f77228c153a3a571e
Pedro Henrique Castro <0000109553547xsp@al.educacao.sp.gov.br>
2024-11-24 14:48:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 8*7?',
+    '56'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'Rússia'
+)
+
+criaCartao(
+    'História',
+    'Em que ano o Brasil declarou independência?',
+    '1822'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o gás mais abundante na atmosfera?',
+    'Nitrogênio'
+)
